bae0f37caec5e9c11e79c60f4dd8fefe1bf2e585
taipioca <142531373+taipioca@users.noreply.github.com>
2024-05-06 21:19:21 -0700

    Update api.js

--- server/api.js
+++ server/api.js
@@ -2,12 +2,14 @@ const express = require("express");
 const db_operations = require("./db_operations");
 const router = express.Router();
 
-router.get("/getlocations", async (req, res)=>{
-    const locations_categories = await db_operations.getLocations()
-    res.send(locations_categories)
-})
-router.get("/getdata", async (req, res)=>{
-    const neighborhoodData = await db_operations.getNeighborhoods()
-    res.send(neighborhoodData)
-})
+router.get("/getlocations", async (req, res) => {
+  console.log("getlocations");
+  const locations_categories = await db_operations.getLocations();
+  res.send(locations_categories);
+});
+router.get("/getdata", async (req, res) => {
+  console.log("getdata");
+  const neighborhoodData = await db_operations.getNeighborhoods();
+  res.send(neighborhoodData);
+});
 module.exports = router;
