4bc3b3cbcce6fe3673c33dfe649456abf5e62738
taipioca <142531373+taipioca@users.noreply.github.com>
2024-05-07 10:28:21 -0700

    Update GenerateLayers.js

--- src/GenerateLayers.js
+++ src/GenerateLayers.js
@@ -1,33 +1,52 @@
-import { PolygonLayer } from '@deck.gl/layers';
-import { COORDINATE_SYSTEM, TileLayer } from 'deck.gl';
-const getLayer = async (neighborhoodData, setNeighborhood, setLocation, setCursor, filter=false) =>{
-
-  if (filter=="Redlining Overlay"){
-      return getRedliningLayer()
+import { PolygonLayer } from "@deck.gl/layers";
+import { COORDINATE_SYSTEM, TileLayer } from "deck.gl";
+const getLayer = async (
+  neighborhoodData,
+  setNeighborhood,
+  setLocation,
+  setCursor,
+  filter = false
+) => {
+  if (filter == "Redlining Overlay") {
+    return getRedliningLayer();
   }
 
-  const neighborhoods = await fetch('src/neighborhoods.json').then((data)=>data.json());
+  const neighborhoods = await fetch("./neighborhoods.json").then((data) =>
+    data.json()
+  );
   const polygonData = [];
   let currentNeighborhood;
   let currentNeighborhoodElvation;
   let currentNeighborhoodColor;
-  Object.keys(neighborhoods).forEach(function(neighborhood_name, _) {
+  Object.keys(neighborhoods).forEach(function (neighborhood_name, _) {
     currentNeighborhood = neighborhoodData[neighborhood_name];
-    currentNeighborhoodElvation = (filter && currentNeighborhood[filter]) ? 
-    (currentNeighborhood[filter]/neighborhoodData.maxes[filter])*1000 
-    : 0
-    currentNeighborhoodColor = (filter && currentNeighborhood[filter]) ? 
-    [80 + (currentNeighborhood[filter]/neighborhoodData.maxes[filter])*150, 30 + (currentNeighborhood[filter]/neighborhoodData.maxes[filter])*90, 110 + (currentNeighborhood[filter]/neighborhoodData.maxes[filter])*150]
-    : [200, 200, 200]
+    currentNeighborhoodElvation =
+      filter && currentNeighborhood[filter]
+        ? (currentNeighborhood[filter] / neighborhoodData.maxes[filter]) * 1000
+        : 0;
+    currentNeighborhoodColor =
+      filter && currentNeighborhood[filter]
+        ? [
+            80 +
+              (currentNeighborhood[filter] / neighborhoodData.maxes[filter]) *
+                150,
+            30 +
+              (currentNeighborhood[filter] / neighborhoodData.maxes[filter]) *
+                90,
+            110 +
+              (currentNeighborhood[filter] / neighborhoodData.maxes[filter]) *
+                150,
+          ]
+        : [200, 200, 200];
     polygonData.push({
-        contours: neighborhoods[neighborhood_name], 
-        neighborhood: neighborhood_name, 
-        color: currentNeighborhoodColor,
-        elavation: currentNeighborhoodElvation,
-        neighborhood_data: currentNeighborhood
-    })
+      contours: neighborhoods[neighborhood_name],
+      neighborhood: neighborhood_name,
+      color: currentNeighborhoodColor,
+      elavation: currentNeighborhoodElvation,
+      neighborhood_data: currentNeighborhood,
+    });
   });
-  
+
   const layer = new PolygonLayer({
     coordinateSystem: COORDINATE_SYSTEM.LNGLAT,
     id: "neighborhoods",
@@ -38,52 +57,63 @@ const getLayer = async (neighborhoodData, setNeighborhood, setLocation, setCurso
     // extruded: filter ? true: false,
     // extruded: filter ? true: false,
     wireframe: true,
-    getPolygon: d => d.contours,
-    getLineColor: filter ? [255, 255, 255]: [0, 0, 0],
-    getLineColor: filter ? [255, 255, 255]: [0, 0, 0],
-    getFillColor: d=> d.color,
+    getPolygon: (d) => d.contours,
+    getLineColor: filter ? [255, 255, 255] : [0, 0, 0],
+    getLineColor: filter ? [255, 255, 255] : [0, 0, 0],
+    getFillColor: (d) => d.color,
     // getElevation: d-> d.elevation,
     // getElevation: d-> d.elevation,
     getLineWidth: 30,
-    onHover: (info, event)=>{
+    onHover: (info, event) => {
       if (info.object) setCursor(true);
-      else setCursor(false);},
-    onClick: (info, event)=>{
-      setNeighborhood({...info.object.neighborhood_data, name: info.object.neighborhood})
-      setLocation({lng: info.coordinate[0], lat: info.coordinate[1]})
+      else setCursor(false);
+    },
+    onClick: (info, event) => {
+      setNeighborhood({
+        ...info.object.neighborhood_data,
+        name: info.object.neighborhood,
+      });
+      setLocation({ lng: info.coordinate[0], lat: info.coordinate[1] });
     },
   });
   return layer;
-}
+};
 
-const getRedliningLayer  = async()=>{
-  const redlining_colors = {"A": [38, 113, 45], "B": [16, 32, 145], "C": [214, 154, 64], "D": [96, 32, 138]}
-      const districts = await fetch('src/redlining.json').then((data)=>data.json());
-      const polygonData = [];
-      let currentDistrictColor;
-      // For each neighborhood calculating the elevation, fill color
-      Object.keys(districts).forEach(function(district, _) {
-          currentDistrictColor = redlining_colors[districts[district].grade]
-          polygonData.push({
-              contours: districts[district].coords, 
-              color: currentDistrictColor,
-          })
-      });
-
-    const layer = new PolygonLayer({
-      coordinateSystem: COORDINATE_SYSTEM.LNGLAT,
-      id: "redlining",
-      data: polygonData,
-      pickable: false,
-      stroked: true,
-      filled: true,
-      wireframe: true,
-      getPolygon: d => d.contours,
-      getLineColor: [255, 255, 255],
-      getFillColor: d=> d.color,
-      getLineWidth: 30,
+const getRedliningLayer = async () => {
+  const redlining_colors = {
+    A: [38, 113, 45],
+    B: [16, 32, 145],
+    C: [214, 154, 64],
+    D: [96, 32, 138],
+  };
+  const districts = await fetch("src/redlining.json").then((data) =>
+    data.json()
+  );
+  const polygonData = [];
+  let currentDistrictColor;
+  // For each neighborhood calculating the elevation, fill color
+  Object.keys(districts).forEach(function (district, _) {
+    currentDistrictColor = redlining_colors[districts[district].grade];
+    polygonData.push({
+      contours: districts[district].coords,
+      color: currentDistrictColor,
     });
-  return layer
-}
+  });
 
-  export default getLayer;
+  const layer = new PolygonLayer({
+    coordinateSystem: COORDINATE_SYSTEM.LNGLAT,
+    id: "redlining",
+    data: polygonData,
+    pickable: false,
+    stroked: true,
+    filled: true,
+    wireframe: true,
+    getPolygon: (d) => d.contours,
+    getLineColor: [255, 255, 255],
+    getFillColor: (d) => d.color,
+    getLineWidth: 30,
+  });
+  return layer;
+};
+
+export default getLayer;
