ff31b50467157e15320fa5358419a718daae14b8
taipioca <142531373+taipioca@users.noreply.github.com>
2024-05-08 16:03:22 -0700

    Move /api directory to the project root. Make path changes in other files due to the move.

--- api/getlocations.js
+++ api/getlocations.js
@@ -1,4 +1,4 @@
-const db_operations = require("../db_operations");
+const db_operations = require("../server/db_operations");
 
 module.exports = async (req, res) => {
   const locations_categories = await db_operations.getLocations();
